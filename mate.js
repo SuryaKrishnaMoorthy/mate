0597d70b4961ba1f16bd44d56a8b29269d7563f8
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-09-27 09:56:22 -0700

    add describe and it function to mate.js

--- mate.js
+++ mate.js
@@ -1,6 +1,24 @@
 const AssertionError = require('assert').AssertionError;
 const chalk = require('chalk')
 
+  let passing = 0;
+  let failing = 0;
+
+  const describe = (desc, cb) => {
+    cb();
+    console.log(`${chalk.green(`${passing} passing`)} , ${chalk.red(`${failing} failing`)}`);
+  }
+
+  const it = (desc, cb) => {
+    try {
+      cb();
+      console.log(`${chalk.green(`\u{2713}`)} ${desc}\n`)
+      passing++;
+    } catch (e) {
+      console.log(`${chalk.red(`\u{2717}`)} AssertionError: ${e.message}`);
+      failing++;
+    }
+  }
 
 module.exports = {
   describe,
